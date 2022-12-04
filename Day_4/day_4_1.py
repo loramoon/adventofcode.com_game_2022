ba2124c1982991def08f08eb87256799e4a46718
loramoon <112502847+loramoon@users.noreply.github.com>
2022-12-04 10:49:46 +0200

    Update day_4_1.py

--- Day_4/day_4_1.py
+++ Day_4/day_4_1.py
@@ -8,12 +8,12 @@ while True:
         break
     first_area, second_area = [], []
     first_range, second_range = line.split(",")
-    first_range_start, first_range_end = first_range.split("-")
-    second_range_start, second_range_end = second_range.split("-")
+    first_range_start, first_range_end = [int(i) for i in first_range.split("-")]
+    second_range_start, second_range_end = [int(i) for i in second_range.split("-")]
 
-    for place in range(int(first_range_start), int(first_range_end)+1):
+    for place in range(first_range_start, first_range_end + 1):
         first_area.append(place)
-    for place in range(int(second_range_start), int(second_range_end)+1):
+    for place in range(second_range_start, second_range_end + 1):
         second_area.append(place)
 
     if set(second_area).issubset(set(first_area)) or set(first_area).issubset(second_area):
