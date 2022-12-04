ef84626d95d5e661abbc7baded72bf0ba437d151
loramoon <112502847+loramoon@users.noreply.github.com>
2022-12-04 02:10:59 +0200

    Update day_1_1.py

--- Day_1/day_1_1.py
+++ Day_1/day_1_1.py
@@ -1,16 +1,14 @@
 file = open('day_1.txt')
-elves = []
-current_elf = 0
+elves = [0]
 
 while True:
     line = file.readline()
     if not line:
         break
     if line != "\n":
-        current_elf += int(line)
+        elves[-1] += int(line)
     else:
-        elves.append(current_elf)
-        current_elf = 0
+        elves.append(0)
 
 print(max(elves)) #frist_task
 
